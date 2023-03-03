eb68615d95784e3159cd826912560c4649b938a2
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-03 14:05:27 -0300

    Add project files.

---
+++ VerificaPrimo/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Digite o número a verificar: ");
+if (!double.TryParse(Console.ReadLine(), out var number))
+{
+    Console.WriteLine("Por favor digite um número válido!");
+}
+else
+{
+    bool primeNumber = true;
+
+    for(int i = 2; i < number; i++) // dividir o numero de 2 a ele -1 se true nao é primo.
+    {
+        if(number % i == 0)
+        {
+            primeNumber = false;
+            break;
+        }
+    }
+
+    if (primeNumber && number > 0)
+        Console.WriteLine("Primo!");
+    else
+        Console.WriteLine("Não Primo!");
+}
+//
